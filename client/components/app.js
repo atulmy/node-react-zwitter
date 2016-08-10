80ac1c7148e5b1aa7683d818ab3c471ed0088b3f
atulmy <atul.12788@gmail.com>
2016-08-10 17:37:45 +0530

    Webpack Hot module reload

--- client/components/app.js
+++ client/components/app.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export default () => {
-    return (
-        <h1>Hello World</h1>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
 }
+
+export default App;
